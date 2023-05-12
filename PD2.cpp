4f023b636593f3958974de40891974d557a84df2
Lineinuniv <daiphat2442003@gmail.com>
2023-05-12 15:22:00 +0700

    struct order

--- PD2.cpp
+++ PD2.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <math.h>
 #include <vector>
+#include <fstream>
+#include <sstream>
 using namespace std;
-struct Point
+struct Point // định nghĩa một điểm
 {
     double x;
     double y;
@@ -11,31 +13,50 @@ struct Point
         this->x = x;
         this->y = y;
     }
+    double distance(Point a)
+    {
+        return sqrt((this->x - a.x) * (this->x - a.x) + (this->y - a.y) * (this->y - a.y));
+    }
 };
 
-double Distance(Point a, Point b)
+struct Order // định nghĩa một đơn hàng
 {
-    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
-}
+    Point pickup;
+    Point delivery;
+    double weight;
+    void setOrder(double x1, double y1, double x2, double y2, double weight)
+    {
+        pickup.setXY(x1, y1);
+        delivery.setXY(x2, y2);
+        this->weight = weight;
+    }
+};
 
-int main()
+// void printOrders(vector<Order> orders){
+//     for (int i = 0; i < orders.size(); i++)
+//     {
+//         cout << orders[i].pickup.x << " " << orders[i].pickup.y << " " << orders[i].delivery.x << " " << orders[i].delivery.y << " " << orders[i].weight;
+//         cout << endl;
+//     }
+// }
+
+int main(int argc, char *agrv[])
 {
-    int n;
-    cin >> n;
-    vector<Point> M;
-    vector<vector<Point>> Dis;
+    ifstream inFile;
+    inFile.open();
+    int n, w;
+    cin >> n >> w;
+    vector<Order> orders;             // vector chứa tất cả các đơn hàng
+    vector<vector<double>> distances; // vector lồng (ma trận) chứa khoảng cách của các điểm
     for (int i = 0; i < n; i++)
     {
-        double x, y;
-        cin >> x >> y;
-        Point point;
-        point.setXY(x, y);
-        M.push_back(point);
+        double x1, y1, x2, y2, weight;
+        cin >> x1 >> y1 >> x2 >> y2 >> weight;
+        Order order;
+        order.setOrder(x1, y1, x2, y2, weight);
+        orders.push_back(order);
     }
-    // for (auto i : M)
-    // {
-    //     cout << i.x << " " << i.y;
-    //     cout << endl;
-    // }
+    // printOrders(orders); kiểm tra dãy các đơn hàng oke chưa bằng cách in ra
+
     return 0;
 }
